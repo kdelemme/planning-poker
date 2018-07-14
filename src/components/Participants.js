78d43b7403edd61019d4ee04d6b07e939b1525a1
Kevin Delemme <kevin.delemme@transferwise.com>
2018-07-14 16:35:10 -0400

    Prefix name with smiley

--- src/components/Participants.js
+++ src/components/Participants.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 
 class Participants extends Component {
+  displayName(participant) {
+    const { voteInProgress } = this.props;
+    if (participant.hasVoted || !voteInProgress) {
+      return `😀 ${participant.name}`;
+    } else {
+      return `🤔 ${participant.name}`;
+    }
+  }
+
   render() {
     const { participants, voteInProgress } = this.props;
     const allVotesReceived = participants.every(p => p.hasVoted);
@@ -10,7 +19,7 @@ class Participants extends Component {
           {participants.map(p => (
             <li key={p.id} className="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 className="my-0">{p.name}</h6>
+                <h6 className="my-0">{this.displayName(p)}</h6>
                 <small className={p.hasVoted && voteInProgress ? "text-success" : "text-muted"}>
                   {voteInProgress ? (p.hasVoted ? "Vote received" : "Awaiting vote") : "Awaiting new round"}
                 </small>
