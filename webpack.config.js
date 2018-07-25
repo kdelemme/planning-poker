9c189dfef8d054c40780a9d0735efe89a0ce0ba1
Kevin Delemme <kevin.delemme@transferwise.com>
2018-07-25 06:16:03 +0100

    Bundle CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,39 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const htmlWebpackConfigForEnv = (env = "dev") =>
-  new HtmlWebpackPlugin({
-    template: `./webpack/index.${env}.html`,
-    filename: "index.html",
-    inject: "body"
-  });
+const htmlWebpackConfig = new HtmlWebpackPlugin({
+  template: process.env.NODE_ENV === "prod" ? "./webpack/index.prod.html" : "./webpack/index.dev.html",
+  filename: "index.html",
+  inject: "body"
+});
 
-module.exports = (env = "dev") => ({
+module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve("dist"),
     filename: "bundle.js"
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, use: { loader: "babel-loader" } }]
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: { loader: "babel-loader" } },
+      {
+        test: /\.css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"]
+      }
+    ]
   },
   externals: {
-    Config: require(`./config.${env}.json`)
+    Config: JSON.stringify(
+      process.env.NODE_ENV === "prod" ? require("./config.prod.json") : require("./config.dev.json")
+    )
   },
   devtool: "eval-source-map",
-  plugins: [htmlWebpackConfigForEnv(env)]
-});
+  plugins: [
+    htmlWebpackConfig,
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
